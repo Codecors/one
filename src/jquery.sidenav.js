862d073fac387665dfbfc80cd07eb0ef2d35ad74
Tony Wang <ziscloud@gmail.com>
2015-09-28 22:54:53 +0800

    implement the plugin feature

--- src/jquery.sidenav.js
+++ src/jquery.sidenav.js
@@ -15,7 +15,7 @@
     'use strict';
 
 
-    var defaults = {};
+    var defaults = {animationSpeed: 500};
 
     /**
      * @constructor
@@ -31,7 +31,45 @@
 
     /** @access private */
     SideNav.prototype.init = function init() {
+        this.$el.on('click', 'li a', function (e) {
+            //Get the clicked link and the next element
+            var $this = $(this);
+            var checkElement = $this.next();
 
+            //Check if the next element is a menu and is visible
+            if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
+                //Close the menu
+                checkElement.slideUp(this.options.animationSpeed, function () {
+                    checkElement.removeClass('menu-open');
+                    //Fix the layout in case the sidebar stretches over the height of the window
+                    //_this.layout.fix();
+                });
+                checkElement.parent('li').removeClass('active');
+            }
+            //If the menu is not visible
+            else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+                //Get the parent menu
+                var parent = $this.parents('ul').first();
+                //Close all open menus within the parent
+                var ul = parent.find('ul:visible').slideUp(this.options.animationSpeed);
+                //Remove the menu-open class from the parent
+                ul.removeClass('menu-open');
+                //Get the parent li
+                var parent_li = $this.parent('li');
+
+                //Open the target menu and add the menu-open class
+                checkElement.slideDown(this.options.animationSpeed, function () {
+                    //Add the class active to the parent li
+                    checkElement.addClass('menu-open');
+                    parent.find('li.active').removeClass('active');
+                    parent_li.addClass('active');
+                });
+            }
+            //if this isn't a link, prevent the page from being redirected
+            if (checkElement.is('.treeview-menu')) {
+                e.preventDefault();
+            }
+        });
     };
 
     /** @access private */
